0c2b3bc1ecd76ecd1291cb8fa9d0ed1c54299e76
ngvthang2011 <ngvthang2011@gmail.com>
2021-06-01 00:10:34 +0700

    feat(mailer) and end project

--- modules/cart/cart.php
+++ modules/cart/cart.php
@@ -58,8 +58,114 @@
 <?php } ?>
 
 <!--	Customer Info	-->
+<?php 
+
+require 'SendMail/src/Exception.php';
+require 'SendMail/src/PHPMailer.php';
+require 'SendMail/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+    if(isset($_POST['email']))
+    {
+        if($_POST['email']!='')
+        {
+            $name=$_POST['name'];
+            $phone=$_POST['phone'];
+            $email=$_POST['email'];
+            $address=$_POST['address'];
+            $html='
+            
+        <div style="border: 1px dotted forestgreen;">
+        <h3 align="center">Thông tin khách hàng</h3>
+        Họ tên:'.$name.' <br>
+        Sđt:'.$phone.' <br>
+        email: '.$email.' <br>
+        địa chỉ : '.$address.'
+        </div>
+        <table style="width: 100%;" >
+        <thead style="background-color: cornflowerblue;">
+            <tr>
+            <th>Mã Sản phẩm</th>
+            <th>Tên Sản Phẩm</th>
+            <th>Số lượng</th>
+            <th>Đơn giá</th>
+            <th>Thành tiền</th>
+            </tr>
+    </thead>
+    <tbody>
+            ';
+
+            $query = mysqli_query($conn, $sql);
+            while($row = mysqli_fetch_assoc($query)) {
+                $html.='
+    <tr>
+            <td>#'.$row['prd_id'].'</td>
+            <td>'.$row['prd_name'].'</td>
+            <td>'.$_SESSION['cart'][$row['prd_id']].'</td>
+            <td>'.number_format($row['prd_price'],0,'','.').'đ</td>
+            <td>'.number_format($_SESSION['cart'][$row['prd_id']]*$row['prd_price'],0,'','.').'đ</td>
+        </tr>
+    ';
+            }
+
+    
+        
+    $html.='
+    <tr style="font-size: 30px; font-weight: bold; color: red;">
+            <td >Tổng tiền</td>
+            <td  colspan="4" align="right">'.number_format($total_price_all,0,'','.').'đ</td>
+        </tr>
+      
+    </tbody>
+    </table>
+    <p align="center" style="font-weight: bold;">Cảm ơn bạn đã mua hàng bên F88 MOBILE SHOP</p>
+    ';
+
+    // Gửi mail
+
+    $mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'thangbadu98@gmail.com';                     // SMTP username
+    $mail->Password   = 'nhhtkfjtmxdczaem';                               // SMTP password
+    $mail->SMTPSecure = 'TLS';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //fix loi font tiếng việt
+    $mail->CharSet = 'UTF-8';
+
+    //Recipients
+    $mail->setFrom('thangbadu98@gmail.com', 'F88 MOBILE SHOP');
+    $mail->addAddress($email, $name);     // Add a recipient
+
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'XÁC NHẬN ĐƠN HÀNG từ F88 MOBILE SHOP';
+    $mail->Body    = $html;
+
+    $mail->send();
+    session_destroy();
+    header('location: index.php?page_layout=success');
+    
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+        }
+    }
+ 
+ ?>
+
 <div id="customer">
-    <form method="post">
+    <form id="frm" method="post">
         <div class="row">
 
             <div id="customer-name" class="col-lg-4 col-md-4 col-sm-12">
@@ -69,10 +175,10 @@
                 <input placeholder="Số điện thoại (bắt buộc)" type="text" name="phone" class="form-control" required>
             </div>
             <div id="customer-mail" class="col-lg-4 col-md-4 col-sm-12">
-                <input placeholder="Email (bắt buộc)" type="text" name="mail" class="form-control" required>
+                <input placeholder="Email (bắt buộc)" type="text" name="email" class="form-control" required>
             </div>
             <div id="customer-add" class="col-lg-12 col-md-12 col-sm-12">
-                <input placeholder="Địa chỉ nhà riêng hoặc cơ quan (bắt buộc)" type="text" name="add"
+                <input placeholder="Địa chỉ nhà riêng hoặc cơ quan (bắt buộc)" type="text" name="address"
                     class="form-control" required>
             </div>
 
@@ -80,7 +186,7 @@
     </form>
     <div class="row">
         <div class="by-now col-lg-6 col-md-6 col-sm-12">
-            <a href="#">
+            <a onclick="buyNow()">
                 <b>Mua ngay</b>
                 <span>Giao hàng tận nơi siêu tốc</span>
             </a>
@@ -94,3 +200,9 @@
     </div>
 </div>
 <!--	End Customer Info	-->
+
+<script>
+    function buyNow() {
+         document.getElementById('frm').submit();
+     }
+</script>
